7da56d3dce9a078623921ce2c41fe19e7e66d65a
Amit Kumar <amitk200415@gmail.com>
2025-01-08 19:04:34 +0530

    frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { SignupFormDemo } from './components/Aceternity/SignupForm/FormComponent'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <div className='m-4'>
+        <SignupFormDemo />
+      </div>
+      <div>
+        <SignupFormDemo />
+      </div>
+    </div>
+  )
+}
+
+export default App
